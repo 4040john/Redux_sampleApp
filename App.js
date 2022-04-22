d7466f87e2bcbe3adfcbdabaaa563ac1cc57ca9c
4040john <yoji4098@gmail.com>
2022-04-22 11:20:58 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <View>
+          <Text>Hello, React Native</Text>
+        </View>
+      </NavigationContainer>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
