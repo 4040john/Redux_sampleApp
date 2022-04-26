d80fb2a75f072428b3c387ee1c848b149415e8b9
4040john <yoji4098@gmail.com>
2022-04-26 17:27:43 +0900

    Update Screens Signin api 추가

--- app/store/actions/User_actions.js
+++ app/store/actions/User_actions.js
@@ -1,16 +1,35 @@
 import {SIGN_IN, SIGN_UP} from '../types';
 
 import axios from 'axios';
-import {SIGNUP} from '../../utils/Misc';
+import {SIGNUP, SIGNIN} from '../../utils/Misc';
 
 export function SignIn(data) {
   console.log('data', data);
+
+  const request = axios({
+    method: 'POST',
+    url: SIGNIN,
+    data: {
+      email: data.email,
+      password: data.password,
+      returnSecureToken: true,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      console.log('response', response.data);
+      return response.data;
+    })
+    .catch(err => {
+      console.log('err', err);
+      alert('Alert Errors');
+      return false;
+    });
   return {
     type: SIGN_IN,
-    payload: {
-      email: data.email,
-      token: data.password,
-    },
+    payload: request,
   };
 }
 export function SignUp(data) {
--- app/utils/Misc.js
+++ app/utils/Misc.js
@@ -1,2 +1,3 @@
 export const APIKEY = `AIzaSyBTQkEWdzIItatzoSSBlLWI29hJOJYjq_g`;
 export const SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIKEY}`;
+export const SIGNIN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${APIKEY}`;
