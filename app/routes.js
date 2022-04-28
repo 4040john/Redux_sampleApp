ba64b64fbbfab632236751420d3d2517c7509703
4040john <yoji4098@gmail.com>
2022-04-28 15:08:14 +0900

    Update Screens 네비게이션 추가 //Logo source 위치관련 수정 필요

--- app/routes.js
+++ app/routes.js
@@ -8,16 +8,55 @@ import SignIn from './components/auth';
 import Diary from './components/diary';
 import News from './components/news';
 
+import DiaryDocu from './components/diary/DiaryDocu';
+import Logo from './utils/Logo';
+
 const AuthStack = createStackNavigator();
 const MainScreenTab = createBottomTabNavigator();
+const DiaryStack = createStackNavigator();
+const NewsStack = createStackNavigator();
+
+const headerConfig = {
+  headerTitleAlign: 'center',
+  headerTintColor: '#fff',
+  headerStyle: {
+    backgroundColor: '37487c5',
+  },
+  headerTitle: <Logo />,
+  headerTitleStyle: {flex: 1, textAlign: 'center'},
+};
 
 const isLoggedIn = false;
+
+const DiaryStackComponent = () => {
+  return (
+    <DiaryStack.Navigator>
+      <DiaryStack.Screen
+        name="Diary"
+        component={Diary}
+        options={headerConfig}
+      />
+      <DiaryStack.Screen
+        name="DiaryDocu"
+        component={DiaryDocu}
+        options={headerConfig}
+      />
+    </DiaryStack.Navigator>
+  );
+};
+const NewsStackComponent = () => {
+  return (
+    <DiaryStack.Navigator>
+      <DiaryStack.Screen name="News" component={News} options={headerConfig} />
+    </DiaryStack.Navigator>
+  );
+};
 
 const AppTabComponent = () => {
   return (
     <MainScreenTab.Navigator>
-      <MainScreenTab.Screen name="Diary" component={Diary} />
-      <MainScreenTab.Screen name="News" component={News} />
+      <MainScreenTab.Screen name="Diary" component={DiaryStackComponent} />
+      <MainScreenTab.Screen name="News" component={NewsStackComponent} />
     </MainScreenTab.Navigator>
   );
 };
